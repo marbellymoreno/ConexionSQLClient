aa6012975ff32543e34671ba775cd9f316ad6bcc
Marbelly <me0763032023@unab.edu.sv>
2024-08-16 14:32:20 -0600

    Agregar archivos de proyecto.

---
+++ CapaConexion/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaConexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-C2JBGHR;Initial Catalog=Northwind;Integrated Security=True;");
+
+            MessageBox.Show("Conexion creada");
+            conexion.Open();
+
+            //------------------------
+
+            String selectFrom = "";
+            selectFrom = selectFrom + "SELECT [CompanyName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactName] " + "\n";
+            selectFrom = selectFrom + "      ,[ContactTitle] " + "\n";
+            selectFrom = selectFrom + "      ,[Address] " + "\n";
+            selectFrom = selectFrom + "      ,[City] " + "\n";
+            selectFrom = selectFrom + "      ,[Region] " + "\n";
+            selectFrom = selectFrom + "      ,[PostalCode] " + "\n";
+            selectFrom = selectFrom + "      ,[Country] " + "\n";
+            selectFrom = selectFrom + "      ,[Phone] " + "\n";
+            selectFrom = selectFrom + "      ,[Fax] " + "\n";
+            selectFrom = selectFrom + "  FROM [dbo].[Customers]";
+
+            //-----------------------
+            SqlCommand comando = new SqlCommand(selectFrom, conexion);
+            SqlDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var customerId = reader[0];
+            }
+
+            MessageBox.Show("Conexion cerrada");
+            conexion.Close();
+        }
+    }
+}
