bd2a846afa58640aed7e0055a684374c6d85087c
Marbelly <me0763032023@unab.edu.sv>
2024-08-30 13:47:49 -0600

    GUIA XVI

--- DatosLayer/CustomerRepository.cs
+++ DatosLayer/CustomerRepository.cs
@@ -71,10 +71,11 @@ namespace DatosLayer
                 selectForID = selectForID + "      ,[Phone] " + "\n";
                 selectForID = selectForID + "      ,[Fax] " + "\n";
                 selectForID = selectForID + "  FROM [dbo].[Customers] " + "\n";
-                selectForID = selectForID + $"  Where CustomerID = '{id}'";
+                selectForID = selectForID + "  Where CustomerID = @customerId";
 
                 using (SqlCommand comando = new SqlCommand(selectForID, conexion))
                 {
+                    comando.Parameters.AddWithValue("customerId", id);
                     var reader = comando.ExecuteReader();
                     Customers customers = null;
                     //validadmos 
